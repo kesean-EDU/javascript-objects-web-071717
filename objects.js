44a84d4cc5daa6a074b26cdd25e1543b71fa5e5e
kewoodh <kesean.woodhouse@gmail.com>
2017-08-28 13:57:21 -0400

    Done.

---
+++ objects.js
@@ -0,0 +1,13 @@
+let playlist = {
+  'artistName': 'songTitle'
+};
+
+function updatePlaylist(playlist, name, title){
+  playlist[name]= title
+  return playlist
+}
+
+function removeFromPlaylist(playlist, name){
+  delete playlist[name]
+  return playlist
+}
